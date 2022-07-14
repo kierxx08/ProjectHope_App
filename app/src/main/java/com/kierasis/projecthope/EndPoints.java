14e8d74256c711560dd8c67a2a08941c2c64ce78
Kier Asis <kier.asis8@gmail.com>
2022-07-14 11:30:22 +0800

    EndPoints remove line 6

--- app/src/main/java/com/kierasis/projecthope/EndPoints.java
+++ app/src/main/java/com/kierasis/projecthope/EndPoints.java
@@ -3,8 +3,8 @@ package com.kierasis.projecthope;
 public class EndPoints {
 
 
-    private static final String ROOT_URL = "https://www.kierasis.me/projecthope/";
-    //private static final String ROOT_URL = "http://192.168.254.2/projecthope/";
+
+    private static final String ROOT_URL = "http://192.168.254.2/projecthope/";
 
     public static final String CHECK_APP_VERSION = ROOT_URL + "app_version.php";
     public static final String GET_PHONE_INFO = ROOT_URL + "get_phone_info.php";
